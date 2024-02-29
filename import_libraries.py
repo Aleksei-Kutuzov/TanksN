92ba9dcf839a35883d6e63345f896cb3e9055b62
Aleksei-Kutuzov <Alekseikutuzov2@yandex.com>
2024-02-29 23:17:54 +0300

    updated

--- import_libraries.py
+++ import_libraries.py
@@ -13,3 +13,48 @@ from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 import sqlite3 as sql
 import time
+
+
+def astar(maze, start :tuple, end):
+    m = [[0] * 16 for i in range(16)]
+    m[start[1]][start[0]] = 1
+    a = copy.copy(maze)
+    end = end[::-1]
+    def make_step(k):
+        for i in range(len(m)):
+            for j in range(len(m[i])):
+                if m[i][j] == k:
+                    if i > 0 and m[i - 1][j] == 0 and a[i - 1][j] == 0:
+                        m[i - 1][j] = k + 1
+                    if j > 0 and m[i][j - 1] == 0 and a[i][j - 1] == 0:
+                        m[i][j - 1] = k + 1
+                    if i < len(m) - 1 and m[i + 1][j] == 0 and a[i + 1][j] == 0:
+                        m[i + 1][j] = k + 1
+                    if j < len(m[i]) - 1 and m[i][j + 1] == 0 and a[i][j + 1] == 0:
+                        m[i][j + 1] = k + 1
+
+    k = 0
+    while m[end[0]][end[1]] == 0:
+        k += 1
+        make_step(k)
+    i, j = end
+    k = m[i][j]
+    the_path = [(i, j)]
+    while k > 1:
+        if i > 0 and m[i - 1][j] == k - 1:
+            i, j = i - 1, j
+            the_path.append((i, j))
+            k -= 1
+        elif j > 0 and m[i][j - 1] == k - 1:
+            i, j = i, j - 1
+            the_path.append((i, j))
+            k -= 1
+        elif i < len(m) - 1 and m[i + 1][j] == k - 1:
+            i, j = i + 1, j
+            the_path.append((i, j))
+            k -= 1
+        elif j < len(m[i]) - 1 and m[i][j + 1] == k - 1:
+            i, j = i, j + 1
+            the_path.append((i, j))
+            k -= 1
+    return the_path[::-1]
